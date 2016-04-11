d739397a7d8759889ee15437062e24121a31a656
Jason <hmilyjenny@gmail.com>
2016-04-11 14:00:51 +0800

    链接云db

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 import Express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
-
+import path from 'path';
 
 var app = new Express();
-app.use(express.static(__dirname + "/public"));
+app.use(Express.static(path.resolve(__dirname, '/public')));
 app.use(bodyParser.json({ limit: '20mb' }));
 
 // MongoDB Connection
 import serverConfig from './config';
 import dummyData from './dummyData';
-mongoose.connect(process.env.MONGOLAB_URI||serverConfig.port, (error) => {
+mongoose.connect(serverConfig.mongoURL, (error) => {
   if (error) {
     console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-    process.exit(1);
+    throw error;
   }
-
   // feed some dummy data in DB.
   dummyData();
 });
 
 // start app
-app.listen(process.env.PORT||serverConfig.port, (error) => {
+var server=app.listen(serverConfig.port, (error) => {
   if (!error) {
     var port = server.address().port;
     console.log("App now running on port", port);
