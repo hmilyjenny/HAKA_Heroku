22263d5af4e05d2bcd8e0c3be247d74109fd0f0b
Jason <hmilyjenny@gmail.com>
2016-04-11 13:19:06 +0800

    modify start command

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-  mongoURL:'mongodb://localhost:27017/haka',
-  port: process.env.PORT || 8000,
+  mongoURL:'mongodb://haka:110701@ds019940.mlab.com:19949/haka',
+  port: process.env.PORT || 8080,
   //secret:'gengshangyicenglou',
   token:{secret:'gengshangyicenglou',expires:10080}
 };
