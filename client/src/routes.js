0cbac9119fa7316237165c7d858184a1d6e54776
Jason <hmilyjenny@gmail.com>
2016-04-12 13:13:37 +0800

    test

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './containers/App';
 import LandingPage from './views/LandingPage/landingpage';
-import LoginRoute from './views/LoginPage/loginroute';
+//import LoginRoute from './views/LoginPage/loginroute';
 
 export default(
     <Route  component={App}>
         <Route path='/' component={LandingPage} />
-        <Route path="/login" component={LoginRoute}/>
-        {/*<Route path="/register" component={RegisterRoute} />
+        {/*<Route path="/login" component={LoginRoute}/>
+        <Route path="/register" component={RegisterRoute} />
         <Route path="/dashboard" component={DashBoardRoute} />*/}
     </Route>
 );
